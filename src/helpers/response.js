cddc8f69cdf69a27d62d04d7c1574b8bb4f0cf9c
Ömer Faruk Küçük <kucukomerf@mail.com>
2021-05-15 02:35:01 +0300

    response helper

--- src/helpers/response.js
+++ src/helpers/response.js
@@ -5,14 +5,20 @@ class Response {
     constructor({ error = false, code = 200, message = '' }) {
         this.error = error;
         this.code = code;
-        this.code_description = httpStatus[]
+        this.code_description = httpStatus[this.code];
         if (message) this.message = message
+
+        return this.getResponse()
     }
 
     getResponse() {
-        let response = {error: this.error}
+        let response = {
+            error: this.error,
+            code: this.code,
+            code_description: this.code_description
+        }
         if (this.message) response.message = this.message;
-
+        return response;
     }
 }
 
